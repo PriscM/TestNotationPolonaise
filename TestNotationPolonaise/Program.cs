ea31630bb119290b4140af88b0324c3a909692a6
Priscillia Morel <priscillia.morel78@gmail.com>
2024-10-01 16:37:52 +0200

    Fonction codée - erreurs à gérer total

--- TestNotationPolonaise/Program.cs
+++ TestNotationPolonaise/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace TestNotationPolonaise
 {
@@ -22,6 +23,59 @@ namespace TestNotationPolonaise
             } while (reponse != carac1 && reponse != carac2);
             return reponse;
         }
+       
+        
+        static float Polonaise(String phrase)
+        {            
+            //sép la formule en élémt ind ds un tableau
+            string[] tab = phrase.Split(' ');                  
+            int nbCases = tab.Length;                      
+           //on vérifie qu'un signe de calcul existe
+
+            for (int n=(nbCases-1); n>=0; n--)
+            {                                              
+                //effectuer les calculs selon le type
+               if(tab[n]=="+"||tab[n]=="-"||tab[n]=="*"||tab[n]=="/")
+                {
+                    float resultat = 0, nb1=0, nb2=0;
+                    try
+                    {
+                    nb1 = float.Parse(tab[n + 1]);
+                    nb2 = float.Parse(tab[n + 2]);
+                    }
+                    catch
+                    {
+                        return float.NaN;
+                    }                    
+                    switch (tab[n])
+                    {
+                        case "+":                           
+                            resultat = nb1 + nb2;
+                            break;
+                        case "-":                            
+                            resultat = nb1 - nb2;
+                            break;
+                        case "*":                            
+                            resultat = nb1 * nb2;
+                            break;
+                        case "/":                            
+                            resultat = nb1/nb2;
+                            break;
+                    }
+                    tab[n] = resultat.ToString();
+                    //on décale ces 2 cases des deux chiffres used à la fin du tableau 
+                    for (int j = n+1; j <=(nbCases - 3); j++)
+                        {
+                            tab[j] = tab[j + 2];                           
+                        }  
+                    //on vide les deux dern cases du tableau
+                        tab[nbCases - 1] = " ";
+                        tab[nbCases - 2] = " ";    
+               }                
+                //renvoyer le résultat                
+            }  
+             return float.Parse(tab[0]);                                  
+        }
 
         /// <summary>
         /// Saisie de formules en notation polonaise pour tester la fonction de calcul
@@ -36,6 +90,7 @@ namespace TestNotationPolonaise
                 Console.WriteLine();
                 Console.WriteLine("entrez une formule polonaise en séparant chaque partie par un espace = ");
                 string laFormule = Console.ReadLine();
+                //contrôle saisie             
                 // affichage du résultat
                 Console.WriteLine("Résultat =  " + Polonaise(laFormule));
                 reponse = saisie("Voulez-vous continuer ?", 'O', 'N');
